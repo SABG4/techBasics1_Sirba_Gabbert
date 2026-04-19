4fbd3e254542c742a30620b78531ca5b2f2068c8
SABG4 <Sirba.A.B.Gabbert@stud.leuphana.de>
2026-04-19 21:41:17 +0200

    Update assignment1.py
    This is my first assignment in Tech Basics1: a small piece of ASCII art.

--- assignments/week1/assignment1.py
+++ assignments/week1/assignment1.py
@@ -1,1 +1,12 @@
 
+#first I looked at some inspiration on asciiart.eu
+#I decided for the whale in the water 
+
+print("""
+       ":"
+     ___:____     |"\/"|
+   ,'        `.    \  /
+   |  0        \___/  |
+~^~^~^~^~^~^~^~^~^~^~^~^~^~
+
+""")
