7d430301ca29a1fc930ff0b2bdc2108d9616706d
SABG4 <Sirba.A.B.Gabbert@stud.leuphana.de>
2026-04-19 22:32:18 +0200

    Update assignment1.py
    This is my first assignment of the Tech Basics 1 course: a small piece of ASCII art.

--- assignments/week1/assignment1.py
+++ assignments/week1/assignment1.py
@@ -1,8 +1,11 @@
 
-#first I looked at some inspiration on asciiart.eu
-#I decided for this little whale in the water 
+# First, I looked at some inspiration on asciiart.eu
+# I chose this little whale in the water spouting a water fountain
+# I used triple quotes to print a multiple-line string and started typing the symbols
 
 print("""
+    .""":""".
+      "":""  
        ":"
      ___:____     |"\/"|
    ,'        `.    \  /
